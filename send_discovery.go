bc378a73121283e0482784fff7e8da6ed0989d9e
peet <peet@you.gay>
2022-04-17 12:06:48 +0200

    fixed bug

--- send_discovery.go
+++ send_discovery.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"time"
@@ -13,14 +14,29 @@ const message = "M-SEARCH * HTTP/1.1\r\n" +
 	"ST: ssdp:all\r\n" +
 	"\r\n"
 
+// https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
+// Get preferred outbound ip of this machine
+func getOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return fmt.Sprintf("%v", localAddr.IP.To4())
+}
+
 func getAllDevices(timeout int, channel chan string) []string {
 	var responses []string
 
 	addr := &net.UDPAddr{IP: net.IPv4(239, 255, 255, 250), Port: 1900}
-	ourAddr, _ := net.ResolveUDPAddr("udp", "192.168.178.30:10000")
+	our := getOutboundIP() + ":1000"
+	ourAddr, _ := net.ResolveUDPAddr("udp", our)
 
 	conn, err := net.ListenUDP("udp", ourAddr)
-	defer conn.Close()
+	//	defer conn.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
